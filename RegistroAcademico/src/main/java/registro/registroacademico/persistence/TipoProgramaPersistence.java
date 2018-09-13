90885d9711c01098241f1dbedcbb34c678d81016
Lenovo <Lenovo@URIELPC>
2018-09-13 13:13:53 -0500

    Se crea la persistencia para la transaccionalidad del tipo de programa. @Uriel Rodríguez Vallarta

---
+++ RegistroAcademico/src/main/java/registro/registroacademico/persistence/TipoProgramaPersistence.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package registro.registroacademico.persistence;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import registro.registroacademico.entities.TipoProgramaEntity;
+
+/**
+ * Clase es usada para manejar la transaccionalidad de la entidad Tipo Programa
+ * @author Uriel Rodríguez Vallarta
+ */
+@Stateless
+public class TipoProgramaPersistence {
+@PersistenceContext(unitName = "registro_RegistroAcademico_war_1.0-SNAPSHOTPU")
+    private EntityManager entityManager;
+    /**
+     * Metodo que lista todos los elementos que se encuentran en la tabla Tipo Programa
+     * @return lista de tipo programa
+     */
+    public List<TipoProgramaEntity> findAll(){
+        Query todos = entityManager.createQuery("select tp from TipoProgramaEntity tp");
+        return todos.getResultList();
+    }
+    /**
+     * Metodo que encuentra un Tipo Programa por su id
+     * @param id
+     * @return tipoprogramaEncontrado
+     */
+    public TipoProgramaEntity find(Long id){
+        TipoProgramaEntity tipoprogramaEncontrado = entityManager.find(TipoProgramaEntity.class, id);
+        return tipoprogramaEncontrado;
+    }
+    /**
+     * Metodo que nos permite crear un Tipo Programa
+     * @param tipoprogramaCrear
+     * @return tipoprogramaCreado
+     */
+    public TipoProgramaEntity create(TipoProgramaEntity tipoprogramaCrear){
+        entityManager.persist(tipoprogramaCrear);
+        return tipoprogramaCrear;
+    }
+    /**
+     * Metodo que nos permite actualizar un Tipo Programa
+     * @param tipoprogramaActualizar
+     * @return tipoprogramaActualizado
+     */
+    public TipoProgramaEntity update(TipoProgramaEntity tipoprogramaActualizar){
+        entityManager.merge(tipoprogramaActualizar);
+        return tipoprogramaActualizar;
+    }
+    /**
+     * Metodo que nos permite eliminar un Tipo Programa
+     * @param id 
+     */
+    public void remove(Long id){
+        TipoProgramaEntity tipoprogramaEliminar = entityManager.find(TipoProgramaEntity.class, id);
+        entityManager.remove(tipoprogramaEliminar);
+    }         
+}
