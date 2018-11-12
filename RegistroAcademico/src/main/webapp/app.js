79edadece6c060d7a5092caf100d290dd0a8168a
andresj90 <42348909+andresj90@users.noreply.github.com>
2018-11-12 13:52:51 -0500

    module horario created

---
+++ RegistroAcademico/src/main/webapp/app.js
@@ -0,0 +1,5 @@
+var app = angular.module("RegistroAcademico", [ 
+    'ngRoute',
+    'ui.router',
+    'HorarioModulo'
+]);
---
+++ RegistroAcademico/src/main/webapp/js/curso/crearActualizar/horario.CrearActualizar.ctrl.js
@@ -0,0 +1,43 @@
+var HorarioModule = angular.module('HorarioModule');
+
+HorarioModule.controller('crearHorarioCtrl', ['$scope', '$http', '$state', function ($scope, $http, $state) {
+        var id = $state.params.id;
+
+        $scope.horario = {};
+
+        if (id) {
+            $http.get('api/horarios/' + id).then(function (response) {
+                $scope.horario = response.data;
+            }, function (error) {
+                console.log(error);
+            });
+        }
+
+        $scope.crearHorario = function () {
+            if ($scope.horario.dia && $scope.horario.hora_inicio && horario.hora_finalizacion) {
+                $http.post('api/horarios', JSON.stringify($scope.horario)).then(function (response) {
+                    //Reinicia la vaariable
+                    $scope.horario = {};
+                    // Nombre de la ruta definida en routes
+                    $state.go('listarHorario');
+                }, function (error) {
+                    console.log(error);
+                });
+            }
+        };
+        
+        $scope.actualizarProducto = function(){
+            if ($scope.horario.dia && $scope.horario.hora_inicio && horario.hora_finalizacion) {
+                $http.put('api/horarios/'+id, JSON.stringify($scope.horario)).then(function(response){
+                    //Reinicia la vaariable
+                    $scope.horario = {};
+                    // Nombre de la ruta definida en routes
+                    $state.go('listarHorario');
+                }, function(error){
+                    console.log(error);
+                });
+            }
+        }
+    }]);
+
+
